460aa5962688bf882f1e5a1ae80d8c849fd5decb
shanajasila <shanajasilam@gmail.com>
2022-11-25 18:26:44 +0530

    search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddFriendComponent } from './add-friend/add-friend.component';
+import { SearchFriendComponent } from './search-friend/search-friend.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddFriendComponent
+    AddFriendComponent,
+    SearchFriendComponent
   ],
   imports: [
     BrowserModule,
