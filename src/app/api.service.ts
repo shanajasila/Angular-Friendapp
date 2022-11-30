facb908d7fc04d810b39574011111070d45c7e0d
shanajasila <shanajasilam@gmail.com>
2022-11-30 10:16:44 +0530

    add friend api

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,4 +11,7 @@ export class ApiService {
   {
     return this.http.get("https://dummyapifriends.herokuapp.com/view")
   }
+  addFriend=(dataTosend:any)=>{
+    return this.http.post("https://dummyapifriends.herokuapp.com/adddata",dataTosend)
+  }
 }
